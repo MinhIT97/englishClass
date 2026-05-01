0b0fb07e457c009acc335e7f9535822dd92d43d7
minh nguyen <minh06081997@gmail.com>
2026-05-01 10:06:31 +0700

    fix docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,13 @@ set -e
 
 # Tự động tối ưu hóa Laravel mỗi lần khởi động
 echo "Optimizing Laravel..."
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan event:cache
+php artisan config:clear
+php artisan config:cache || true
+php artisan route:clear
+php artisan route:cache || true
+php artisan view:clear
+php artisan view:cache || true
+php artisan event:clear || true
 
 # Tự động chạy Migration nếu là container 'app'
 if [ "$RUN_MIGRATIONS" = "true" ]; then
